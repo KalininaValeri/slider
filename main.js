551f2e82b19b42d364df6b6638c24952fb5e3063
KalininaValeri <kalinina.valeri@gmail.com>
2016-02-12 15:55:58 +0600

    changed the background .active and :hover

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**
  * Created by lera on 2/12/16.
  */
-//$(function() {
-//    $('#slider').cycle({
-//        fx:     'shuffle',
-//        speed:  'fast',
-//        timeout: 3000,
-//        pager:  '#switch',
-//    });
-//});
+$(function() {
+    $('#slider').cycle({
+        fx:     'shuffle',
+        speed:  'fast',
+        timeout: 3000,
+        pager:  '#switch',
+    });
+});
